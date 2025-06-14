b70848f5a614bc2ee02cc05e61f96365dfb07072
mohamedjagne <mohamedjagne110@gmail.com>
2025-06-14 17:04:08 +0300

    account statement export functionality and transaction currency in account statement

--- resources/views/backend/user/reports/exports/account_statement.blade.php
+++ resources/views/backend/user/reports/exports/account_statement.blade.php
@@ -5,7 +5,6 @@
             <th>{{ _lang('Description') }}</th>
             <th>{{ _lang('Debit') }}</th>
             <th>{{ _lang('Credit') }}</th>
-            <th>{{ _lang('Balance') }}</th>
         </tr>
     </thead>
     <tbody>
@@ -18,49 +17,18 @@
             <td>
                 @if($transaction->dr_cr == 'dr')
                 @if($account->currency != NULL)
-                {{ $transaction->base_currency_amount }}
+                {{ $transaction->transaction_amount }}
                 @else
-                {{ $transaction->base_currency_amount }}
+                {{ $transaction->transaction_amount }}
                 @endif
                 @endif
             </td>
             <td>
                 @if($transaction->dr_cr == 'cr')
                 @if($account->currency != NULL)
-                {{ $transaction->base_currency_amount }}
+                {{ $transaction->transaction_amount }}
                 @else
-                {{ $transaction->base_currency_amount }}
-                @endif
-                @endif
-            </td>
-            <td>
-                @if($account->dr_cr == 'dr')
-                @if($index == 0)
-                @if($account->currency != NULL)
-                {{ $transaction->base_currency_amount }}
-                @else
-                {{ $transaction->base_currency_amount }}
-                @endif
-                @else
-                @if($account->currency != NULL)
-                {{ $account->transactions->where('dr_cr', 'cr')->take($index + 1)->sum('base_currency_amount') - $account->transactions->where('dr_cr', 'dr')->take($index + 1)->sum('base_currency_amount') }}
-                @else
-                {{ $account->transactions->where('dr_cr', 'dr')->take($index + 1)->sum('base_currency_amount') - $account->transactions->where('dr_cr', 'cr')->take($index + 1)->sum('base_currency_amount') }}
-                @endif
-                @endif
-                @else
-                @if($index == 0)
-                @if($account->currency != NULL)
-                {{ $transaction->base_currency_amount }}
-                @else
-                {{ $transaction->base_currency_amount}}
-                @endif
-                @else
-                @if($account->currency != NULL)
-                {{ $account->transactions->where('dr_cr', 'cr')->take($index + 1)->sum('base_currency_amount') - $account->transactions->where('dr_cr', 'dr')->take($index + 1)->sum('base_currency_amount') }}
-                @else
-                {{ $account->transactions->where('dr_cr', 'cr')->take($index + 1)->sum('base_currency_amount') - $account->transactions->where('dr_cr', 'dr')->take($index + 1)->sum('base_currency_amount') }}
-                @endif
+                {{ $transaction->transaction_amount }}
                 @endif
                 @endif
             </td>
@@ -74,18 +42,18 @@
             <td>
                 <span>
                     @if($account->currency != NULL)
-                    {{ $account->transactions->where('dr_cr', 'dr')->sum('base_currency_amount') }}
+                    {{ $account->transactions->where('dr_cr', 'dr')->sum('transaction_amount') }}
                     @else
-                    {{ $account->transactions->where('dr_cr', 'dr')->sum('base_currency_amount') }}
+                    {{ $account->transactions->where('dr_cr', 'dr')->sum('transaction_amount') }}
                     @endif
                     </span>
             </td>
             <td>
                 <span>
                     @if($account->currency != NULL)
-                    {{ $account->transactions->where('dr_cr', 'cr')->sum('base_currency_amount') }}
+                    {{ $account->transactions->where('dr_cr', 'cr')->sum('transaction_amount') }}
                     @else
-                    {{ $account->transactions->where('dr_cr', 'cr')->sum('base_currency_amount') }}
+                    {{ $account->transactions->where('dr_cr', 'cr')->sum('transaction_amount') }}
                     @endif
                 </span>
             </td>
@@ -94,16 +62,16 @@
                     @if($account->currency != NULL)
                     <!-- if account dr_cr = cr -->
                     @if($account->dr_cr == 'cr')
-                    {{ $account->transactions->where('dr_cr', 'cr')->sum('base_currency_amount') - $account->transactions->where('dr_cr', 'dr')->sum('base_currency_amount') }}
+                    {{ $account->transactions->where('dr_cr', 'cr')->sum('transaction_amount') - $account->transactions->where('dr_cr', 'dr')->sum('transaction_amount') }}
                     @else
-                    {{ $account->transactions->where('dr_cr', 'dr')->sum('base_currency_amount') - $account->transactions->where('dr_cr', 'cr')->sum('base_currency_amount') }}
+                    {{ $account->transactions->where('dr_cr', 'dr')->sum('transaction_amount') - $account->transactions->where('dr_cr', 'cr')->sum('transaction_amount') }}
                     @endif
                     @else
                     <!-- if account dr_cr = cr -->
                     @if($account->dr_cr == 'cr')
-                    {{ $account->transactions->where('dr_cr', 'cr')->sum('base_currency_amount') - $account->transactions->where('dr_cr', 'dr')->sum('base_currency_amount') }}
+                    {{ $account->transactions->where('dr_cr', 'cr')->sum('transaction_amount') - $account->transactions->where('dr_cr', 'dr')->sum('transaction_amount') }}
                     @else
-                    {{ $account->transactions->where('dr_cr', 'dr')->sum('base_currency_amount') - $account->transactions->where('dr_cr', 'cr')->sum('base_currency_amount') }}
+                    {{ $account->transactions->where('dr_cr', 'dr')->sum('transaction_amount') - $account->transactions->where('dr_cr', 'cr')->sum('transaction_amount') }}
                     @endif
                     @endif
                 </span>
